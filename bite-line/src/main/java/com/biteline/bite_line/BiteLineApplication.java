d473ad61f9092cb95627ceeeb031a34e8675759c
Joseph Martin Garado <garadojosephmartin98@gmail.com>
2024-11-23 09:32:10 +0800

    bite line main - server-  commit

---
+++ bite-line/src/main/java/com/biteline/bite_line/BiteLineApplication.java
@@ -0,0 +1,13 @@
+package com.biteline.bite_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteLineApplication.class, args);
+	}
+
+}
---
+++ bite-line/src/test/java/com/biteline/bite_line/BiteLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biteline.bite_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
